a3c685d422c56b696c5aa2104d03d2a1d3d3838b
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-03-23 14:45:32 -0700

    create react app

---
+++ src/components/MonsterList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+class MonsterList extends Component {
+    state = { monsters: []}
+
+    componentWillMount() {
+        
+    }
+
+    render() { 
+        return (  )
+    }
+}
+ 
+export default MonsterList;
