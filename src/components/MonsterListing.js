5744dbbe6549615735b62f971509113b2d10e896
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-03-23 17:22:52 -0700

    add modal to MonsterListing

--- src/components/MonsterListing.js
+++ src/components/MonsterListing.js
@@ -1,11 +1,52 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, TouchableHighlight, Modal } from 'react-native';
 
-const MonsterListing = ({ monster }) => {
-    const { name, url } = monster
-    return (
-        <Text>{name}</Text>
-    )
+class MonsterListing extends Component {
+    state = {
+        modalVisible: false,
+    }
+
+    setModalVisible(visible) {
+        this.setState({ modalVisible: visible });
+    }
+
+    render() {
+        const { name, url } = this.props.monster
+        return (
+            <View>
+
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        alert('Modal has been closed.');
+                    }}>
+                    <View 
+                    // style={{ marginTop: 22 }}
+                    >
+                        <View>
+                            <Text>Hello World!</Text>
+
+                            <TouchableHighlight
+                                onPress={() => {
+                                    this.setModalVisible(!this.state.modalVisible);
+                                }}>
+                                <Text>Hide Modal</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </Modal>
+
+                <TouchableHighlight
+                    onPress={() => {
+                        this.setModalVisible(true);
+                    }}>
+                    <Text>{name}</Text>
+                </TouchableHighlight>
+            </View>
+        )
+    }
 }
 
 export default MonsterListing;
