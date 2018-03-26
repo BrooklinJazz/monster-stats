cfd45a5c54722fc49ed4ab8d83515590b934cc23
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-03-26 00:47:51 -0700

    render skills inline

--- src/components/MonsterStats.js
+++ src/components/MonsterStats.js
@@ -18,12 +18,26 @@ const MonsterStats = ({ stat }) => {
         wisdom: wis,
         charisma: cha,
         // START NOTE "CONDITIONAL RENDERING" the following have to be conditionally rendered
+        athletics,
+        acrobatics,
+        sleight_of_hand,
         stealth,
-        survival,
-        perception,
+        arcana,
+        history,
+        investigation,
+        nature,
+        religion,
+        animal_handling,
         insight,
-        // Below values default to empty string value
-        // NOTE may be important when testing their presence for conditional rendering
+        medicine,
+        perception,
+        survivial,
+        deception,
+        intimidation,
+        performance,
+        persuasion,
+        // NOTE may be important when testing their presence for conditional rendering, 
+        // while the above values defaulted to undefined, the below values default to empty strings or arrays
         damage_vulnerabilities,
         damage_resistances,
         damage_immunities,
@@ -41,6 +55,26 @@ const MonsterStats = ({ stat }) => {
         // END NOTE "ARRAYS"
 
     } = stat
+    const skills = [
+        { mod: athletics, string: 'Athletics' },
+        { mod: acrobatics, string: 'Acrobatics' },
+        { mod: sleight_of_hand, string: 'Sleight Of Hand' },
+        { mod: stealth, string: 'Stealth' },
+        { mod: arcana, string: 'Arcana' },
+        { mod: history, string: 'History' },
+        { mod: investigation, string: 'Investigation' },
+        { mod: nature, string: 'Nature' },
+        { mod: religion, string: 'Religion' },
+        { mod: animal_handling, string: 'Animal Handling' },
+        { mod: insight, string: 'Insight' },
+        { mod: medicine, string: 'Medicine' },
+        { mod: perception, string: 'Perception' },
+        { mod: survivial, string: 'Survival' },
+        { mod: deception, string: 'Deception' },
+        { mod: intimidation, string: 'Intimidation' },
+        { mod: performance, string: 'Performance' },
+        { mod: persuasion, string: 'Persuasion' },
+    ]
     return (
         <View>
             <Text>{name}</Text>
@@ -51,7 +85,20 @@ const MonsterStats = ({ stat }) => {
             <Text>{speed}</Text>
             {/* TODO Add Stat Stylings*/}
             <Text>{str}</Text>
-            </View>
+            <Text>{dex}</Text>
+            <Text>{con}</Text>
+            <Text>{int}</Text>
+            <Text>{wis}</Text>
+            <Text>{cha}</Text>
+            {/*Skills*/}
+            <Text>
+                {
+                    skills.map(skill => (
+                        typeof skill.mod !== 'undefined' && <Text key={skill.string}>{skill.string} +{skill.mod} </Text>
+                    ))
+                }
+            </Text>
+        </View>
     )
 }
 
